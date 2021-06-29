498dbfa4cc805207d8992b0ec98d9d3ed2949646
Antonio Gormley <antoniogormley@gmail.com>
2021-06-29 14:52:32 +0100

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Antonio Gormley on 29/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
